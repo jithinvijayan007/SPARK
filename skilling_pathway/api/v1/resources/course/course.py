db53c09526b46a9975e9e944dc28bd659d38e004
Jithin <jithin.v@pacewisdom.com>
2023-03-31 17:53:08 +0530

    minor changes

--- skilling_pathway/api/v1/resources/course/course.py
+++ skilling_pathway/api/v1/resources/course/course.py
@@ -28,22 +28,9 @@ class CourseList(API_Resource):
             url = "https://lms.samhita.org//webservice/rest/server.php?wstoken=9b90383be92709097c2edb05a1dfa7b5&wsfunction=core_course_get_courses&moodlewsrestformat=json"
 
             payload={}
-            headers = {
-            'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-            'Pragma': 'no-cache',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-ch-ua-mobile': '?0',
-            'access-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4MDI2MTEyNiwianRpIjoiOTQxMTc2NjUtZWZjZC00MTYwLWJmNDgtOTI1YmM0NTY3MWM5IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjBkYzM2M2UxLThhZmUtNDcxMS05OGRmLTBjOWI3ZmIwZDliMyIsIm5iZiI6MTY4MDI2MTEyNiwiZXhwIjoxNjgwMzQ3NTI2fQ.II4AEJ_pfXClo7lvvThW9Ig7RBU687WXQ4AE7-3NK94',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-            'Accept': 'application/json, text/plain, /',
-            'Cache-Control': 'no-cache, no-store, must-revalidate',
-            'Referer': 'http://localhost:5400/',
-            'Expires': '0'
-            }
+            headers = {}
 
             response = requests.request("GET", url, headers=headers, data=payload)
-
-            print(response.text)
             result = response.json()
 
 
