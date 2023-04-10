7769b3027f42d39603bc1fda59bb80733e041fc3
Nishad <nishad@PW083.local>
2023-04-10 11:27:28 +0530

    resume parser added

--- skilling_pathway/api/v1/resources/resume_builder/resume_builder.py
+++ skilling_pathway/api/v1/resources/resume_builder/resume_builder.py
@@ -17,10 +17,12 @@ from .parser_helper import (
     resume_parser,
     resume_filter_parser,    
 )
-# from pyresparser import ResumeParser
+from pyresparser import ResumeParser
 from skilling_pathway.api.v1.resources.Resource import API_Resource, NameSpace
 api = NameSpace('ResumeBuilder')
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))))
+RESUME_DIR = os.path.join(os.path.join(BASE_DIR,'static'),'resumes')
 
 class ResumeBuilderAPI(API_Resource):
     # @authenticate
@@ -64,10 +66,9 @@ class ResumeBuilderAPI(API_Resource):
                 "type": "custom_error"
             }, 400
 
-# def show_data(data):
-#     path = 'https://s3revive.s3.amazonaws.com/uploads/aaa.pdf'
-#     data = ResumeParser(path).get_extracted_data()
-#     return data
+def resume_parser(path):
+    data = ResumeParser(path).get_extracted_data()
+    return data
 
 class UploadResume(API_Resource):
     @api.expect(resume_parser)
@@ -76,9 +77,19 @@ class UploadResume(API_Resource):
             resume_file = request.files['resume']
              #This prints the file name of the uploaded file
             print(resume_file.filename)
-            #I want to save the uploaded file as logo.png. No matter what the uploaded file name was.
-            resume_file.save(resume_file.filename)
-            #print(show_data(data))
+            resume_path = os.path.join(RESUME_DIR,resume_file.filename)
+            resume_file.save(resume_path)
+            json_data = resume_parser(resume_path)
+            
+            if os.path.exists(resume_path):
+                os.remove(resume_path)
+            
+            return {
+                "message": "Resume Created succefully",
+                "status": True,
+                "data": json_data
+            }, 200
+            
         except Exception as e:
             import traceback
             print(e)
